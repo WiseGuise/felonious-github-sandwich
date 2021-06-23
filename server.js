ddbb64d74f526b4d7b8a67b745ab9386fab71e89
Glitch (outrageous-wry-town) <none>
2021-06-23 01:18:52 +0000

    💈🏙 Checkpoint ./server.js:33002783/12

--- server.js
+++ server.js
@@ -31,3 +31,5 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get()
